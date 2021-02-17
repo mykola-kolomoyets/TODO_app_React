2e5cee5eb4052b6eac1e8f89f814534aba06da90
Kolomoyets473 <kolomoyets473@gmail.com>
2021-02-17 20:51:47 +0300

    v0.1.1
    - Created basic element with structure of todo-list: title, search field and todo-items
    - Added clases to DOM elements with BEM methodology

--- todo_list/src/index.js
+++ todo_list/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const root = document.getElementById('root');
 
-const el = <h1>Hello world</h1>;
+const el = (
+  <div class="todo-app">
+    <h1 className="todo-app__title">REACT TODO APP</h1>
+    <input type="text"/>
+    <ul className="todo-app__list">
+      <li className="todo-app__list--item">Item1</li>
+      <li className="todo-app__list--item">Item2</li>
+      <li className="todo-app__list--item">Item3</li>
+      <li className="todo-app__list--item">Item4</li>
+      <li className="todo-app__list--item">Item5</li>
+    </ul>
+  </div>
+);
 
 ReactDOM.render(
   <React.StrictMode>
