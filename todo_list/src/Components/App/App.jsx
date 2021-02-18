b5bcca8213289b0ceca77c66d4923610050888ab
Kolomoyets473 <kolomoyets473@gmail.com>
2021-02-18 14:14:22 +0300

    v0.5
    - Changed the data structure of todo list items - array of objects instead of array of arrays with comfortable names for initialization of todo-list item through the Spread-operator
    - Relocated the data to the root of the app - to the App component
    - Changed the TodoList component which gets dta through the props and added the properties of text and priority through the Spread operator

--- todo_list/src/Components/App/App.jsx
+++ todo_list/src/Components/App/App.jsx
@@ -6,11 +6,46 @@ import TodoSearch from './../TodoSearch/TodoSearch';
 import TodoTitle from './../TodoTitle/TodoTitle';
 
 const App = () => {
+  // const items = [
+  //   ["Learn React", true],
+  //   ["Make TODO App", true],
+  //   ["Create more portfolio projects", false],
+  //   ["Pass the interview", true],
+  //   ["Get the job", true],
+  //   ["Drink Coffee", false]
+  // ];
+
+  const todoData = [
+    {
+      label: "Learn React",
+      important: true
+    },
+    {
+      label: "Make TODO App",
+      important: true
+    },
+    {
+      label: "Create more portfolio projects",
+      important: false
+    },
+    {
+      label: "Pass the interview",
+      important: true
+    },
+    {
+      label: "Get the job",
+      important: true
+    },
+    {
+      label: "Drink Coffee",
+      important: false
+    },
+  ]
   return (
-    <div class="todo-app">
+    <div className="todo-app">
       <TodoTitle />
       <TodoSearch />
-      <TodoList />
+      <TodoList todoData={todoData}/>
     </div>
   )
 }
--- todo_list/src/Components/TodoList/TodoList.jsx
+++ todo_list/src/Components/TodoList/TodoList.jsx
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom';
 
 import TodoListItem from './../TodoListItem/TodoListItem';
 
-const TodoList = () => {
-  const items = [
-    ["Learn React", true],
-    ["Make TODO App", true],
-    ["Create more portfolio projects", false],
-    ["Pass the interview", true],
-    ["Get the job", true],
-    ["Drink Coffee", false]
-  ];
-
-  const todoListItems = items.map(item => <TodoListItem 
-    className="todo-app__list--item" label={item[0]}
-    important={item[1]}
-  />);
+const TodoList = (props) => {
+  const todoListItems = props.todoData.map((itemData) => {
+    return (
+      <TodoListItem
+        className="todo-app__list--item" 
+        {...itemData}
+      />
+    );
+  });
 
   return (
     <ul className="todo-app__list">
